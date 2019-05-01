eeb62b6a7e565cfc5cf9b3b9081731dc88169e4f
Yi Chun Lin <yichun212@gmail.com>
2019-05-01 18:59:49 -0400

    feat/scrapper - Add models.

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+     name = models.CharField(max_length=250)
+
+class Book(models.Model):
+     book = models.ForeignKey(Category, on_delete=models.CASCADE)
+     name = models.CharField(max_length=250)
+     price = models.DecimalField(max_digits=5, decimal_places=2)
+     in_stock = models.IntegerField()
+     valoration = models.IntegerField()
+     description = models.CharField(max_length=2500)
+
+class ExtraInformation(models.Model):
+     extra_information = models.ForeignKey(Book, on_delete=models.CASCADE)
+     upc = models.CharField(max_length=250)
+     product_type = models.CharField(max_length=250)
+     price = models.DecimalField(max_digits=5, decimal_places=2)
+     price_with_tax = models.DecimalField(max_digits=5, decimal_places=2)
+     tax = models.DecimalField(max_digits=5, decimal_places=2)
+     availability = models.IntegerField()
+     reviews = models.IntegerField()
