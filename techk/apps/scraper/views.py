0f48577f3332e811b9661e7a6cd012a1abf0c19a
Yi Chun Lin <yichun212@gmail.com>
2019-05-01 19:05:25 -0400

    feat/scrapper - Add views.

--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .usecases import scrapy
+from .models import Category
 
 # Create your views here.
+def index(request):
+    #scrapy()
+    return render(request, 'index.html')
+
+def ajax(request):
+    categories = Category.objects.all()
+    template = loader.get_template('ajax.html')
+    context = {
+        'categories': categories,
+    }
+    return HttpResponse(template.render(context, request))
