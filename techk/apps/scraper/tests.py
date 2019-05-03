443cc26fb00af9128eef8b2634d7aadb0c690582
Yi Chun Lin <yichun212@gmail.com>
2019-05-03 00:05:37 -0400

    feat/scrapper - WIP test.

--- techk/apps/scraper/tests.py
+++ techk/apps/scraper/tests.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from .models import Category
+
 # Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='Test Category')
+
+    def test_category_created_successfully(self):
+        category = Category.objects.get(name="Test Category")
+        self.assertEqual(category, 'The cat says "meow"')
