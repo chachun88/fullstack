378980d09608c6192dc9c78ff7809860e05e2e61
Yi Chun Lin <yichun212@gmail.com>
2019-05-02 20:39:34 -0400

    feat/scrapper - Add a simple react table.

--- techk/apps/scraper/src/components/App.js
+++ techk/apps/scraper/src/components/App.js
@@ -1,18 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from 'axios'
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      postContent: []
+    };
+  }
+  componentDidMount() {
     axios.get('/scraper/ajax')
-      .then(function (response) {
-        console.log(response);
+      .then(response => {
+        this.setState({
+          postContent: response.data
+        })
     })
   }
   render() {
     return (
       <div>
+        <ReactTable
+          data={this.state.postContent}
+          columns={[
+            {
+              Header: "ID",
+              accessor: "id",
+              maxWidth: 100,
+            },
+            {
+              Header: "Name",
+              accessor: "name",
+              maxWidth: 200
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          filterable
+        />
       </div>
     );
   }
--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import JsonResponse
+
 from .usecases import scrapy
 from .models import Category
 
@@ -13,9 +15,5 @@ def index(request):
     return render(request, 'index.html')
 
 def ajax(request):
-    categories = Category.objects.all()
-    template = loader.get_template('ajax.html')
-    context = {
-        'categories': categories,
-    }
-    return HttpResponse(template.render(context, request))
+    categories = list(Category.objects.values())
+    return JsonResponse(categories, safe=False)
